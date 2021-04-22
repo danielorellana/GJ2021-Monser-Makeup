9cfa7f036dd6e696ac5e3c3a9f1d355f7d6cb947
Daniel Orellana <write@danielorellana.ca>
2021-04-22 11:20:57 -0400

    gh-pages build

--- game.js
+++ game.js
@@ -134,16 +134,14 @@ class Level extends Phaser.Scene {
         /* END-USER-CTR-CODE */
     }
     editorCreate() {
-        // dino
-        const dino = this.add.image(400, 245.50984430371858, "dino");
-        // text_1
-        const text_1 = this.add.text(400, 406, "", {});
-        text_1.setOrigin(0.5, 0);
-        text_1.text = "Phaser 3 + Phaser Editor 2D + TypeScript";
-        text_1.setStyle({ "fontFamily": "arial", "fontSize": "3em" });
-        // dino (components)
-        new PushOnClick(dino);
-        dino.emit("components-awake");
+        // background
+        const background = this.add.rectangle(400, 286, 900, 600);
+        background.isFilled = true;
+        background.fillColor = 5194562;
+        // rectangle
+        const rectangle = this.add.rectangle(400, 541, 900, 128);
+        rectangle.isFilled = true;
+        rectangle.fillColor = 15329253;
     }
     /* START-USER-CODE */
     // Write your code here.
