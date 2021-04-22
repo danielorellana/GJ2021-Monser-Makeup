7cc882aac695cfa6f955b52f86a3ea787031df18
David Grenzowski <david.grenzowski@homerlearning.com>
2021-04-22 16:58:25 -0400

    first pass on painting

--- scenes/Level.ts
+++ scenes/Level.ts
@@ -16,9 +16,27 @@ class Level extends Phaser.Scene {
 	editorCreate() {
 		
 		// dino
-		const dino = this.add.image(400, 245.50984430371858, "dino");
+		const dino = this.add.image(360, 189, "dino");
 		dino.scaleX = -0.9920000064349614;
 		dino.scaleY = 0.9079212765992528;
+		
+		// pig_customer
+		const pig_customer = this.add.container(55, 0);
+		
+		// image_1
+		const image_1 = this.add.image(0, 0, "pig_image");
+		image_1.setOrigin(0, 0);
+		pig_customer.add(image_1);
+		
+		// paintlayer
+		const paintlayer = this.add.container(0, 0);
+		pig_customer.add(paintlayer);
+		
+		// image
+		const image = this.add.image(0, 0, "pig_lines");
+		image.setOrigin(0, 0);
+		image.visible = false;
+		pig_customer.add(image);
 		
 		// text_1
 		const text_1 = this.add.text(400, 406, "", {});
@@ -29,6 +47,12 @@ class Level extends Phaser.Scene {
 		// dino (components)
 		new PushOnClick(dino);
 		dino.emit("components-awake");
+		
+		// paintlayer (components)
+		const paintlayerPaintLayer = new PaintLayer(paintlayer);
+		paintlayerPaintLayer.brush = "brush_default";
+		paintlayerPaintLayer.mask_id = "pig_mask";
+		paintlayer.emit("components-awake");
 	}
 	
 	/* START-USER-CODE */
