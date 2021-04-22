7b8a6a36a4363b1022538bc0e217d44538ec9c4c
David Grenzowski <david.grenzowski@homerlearning.com>
2021-04-22 16:43:18 -0400

    gh-pages build

--- game.js
+++ game.js
@@ -165,14 +165,37 @@ class Level extends Phaser.Scene {
         /* END-USER-CTR-CODE */
     }
     editorCreate() {
-        // background
-        const background = this.add.rectangle(400, 286, 900, 600);
-        background.isFilled = true;
-        background.fillColor = 5194562;
-        // rectangle
-        const rectangle = this.add.rectangle(400, 541, 900, 128);
-        rectangle.isFilled = true;
-        rectangle.fillColor = 15329253;
+        // dino
+        const dino = this.add.image(360, 189, "dino");
+        dino.scaleX = -0.9920000064349614;
+        dino.scaleY = 0.9079212765992528;
+        // pig_customer
+        const pig_customer = this.add.container(55, 0);
+        // image_1
+        const image_1 = this.add.image(0, 0, "pig_image");
+        image_1.setOrigin(0, 0);
+        pig_customer.add(image_1);
+        // paintlayer
+        const paintlayer = this.add.container(0, 0);
+        pig_customer.add(paintlayer);
+        // image
+        const image = this.add.image(0, 0, "pig_lines");
+        image.setOrigin(0, 0);
+        image.visible = false;
+        pig_customer.add(image);
+        // text_1
+        const text_1 = this.add.text(400, 406, "", {});
+        text_1.setOrigin(0.5, 0);
+        text_1.text = "Phaser 3 + Phaser Editor 2D + TypeScript";
+        text_1.setStyle({ "fontFamily": "arial", "fontSize": "3em" });
+        // dino (components)
+        new PushOnClick(dino);
+        dino.emit("components-awake");
+        // paintlayer (components)
+        const paintlayerPaintLayer = new PaintLayer(paintlayer);
+        paintlayerPaintLayer.brush = "brush_default";
+        paintlayerPaintLayer.mask_id = "pig_mask";
+        paintlayer.emit("components-awake");
     }
     /* START-USER-CODE */
     // Write your code here.
