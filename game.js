ebc103fad34297db9de52ac28fe10559f1f72fba
Daniel Orellana <write@danielorellana.ca>
2021-04-26 00:07:15 -0400

    gh-pages build

--- game.js
+++ game.js
@@ -333,6 +333,27 @@ class PushOnClick extends UserComponent {
 // You can write more code here
 // You can write more code here
 /* START OF COMPILED CODE */
+class StartAnimation extends UserComponent {
+    constructor(gameObject) {
+        super(gameObject);
+        this.animation_key = "";
+        this.gameObject = gameObject;
+        gameObject["__StartAnimation"] = this;
+        /* START-USER-CTR-CODE */
+        /* END-USER-CTR-CODE */
+    }
+    static getComponent(gameObject) {
+        return gameObject["__StartAnimation"];
+    }
+    /* START-USER-CODE */
+    awake() {
+        this.gameObject.play(this.animation_key);
+    }
+}
+/* END OF COMPILED CODE */
+// You can write more code here
+// You can write more code here
+/* START OF COMPILED CODE */
 class Timer extends UserComponent {
     constructor(gameObject) {
         super(gameObject);
@@ -363,6 +384,8 @@ class Timer extends UserComponent {
     awake() {
         this.scene.events.on(GameEvent.GAME_START, this.startTimer, this);
         this.gameObject.bringToTop(this.icon);
+        this.icon.y += 20;
+        this.gameObject.y -= 20;
     }
     startTimer() {
         this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.updateTimer, this);
@@ -449,30 +472,32 @@ class Level extends Phaser.Scene {
     constructor() {
         super("Level");
         this.customers_ids = ["pig", "frank", "steve", "goblin", "elf"];
-        this.grades = [];
-        this.makeup_colors = [];
         this.customer_index = -1;
         this.starting_score = 0;
+        this.grades = [];
+        this.makeup_colors = [];
         /* START-USER-CTR-CODE */
         // Write your code here.
         /* END-USER-CTR-CODE */
     }
     editorCreate() {
-        // mirror_scene_frame
-        this.add.image(400, 300, "mirror_scene_frame");
-        // rectangle
-        const rectangle = this.add.rectangle(273, 251, 128, 128);
-        rectangle.scaleX = 3.4764732519125743;
-        rectangle.scaleY = 3.2089789491501506;
-        rectangle.isFilled = true;
-        rectangle.fillColor = 7627610;
+        // mirror_bg
+        const mirror_bg = this.add.rectangle(273, 251, 128, 128);
+        mirror_bg.scaleX = 3.4764732519125743;
+        mirror_bg.scaleY = 3.2089789491501506;
+        mirror_bg.isFilled = true;
+        mirror_bg.fillColor = 7627610;
         // customer_container
         const customer_container = this.add.container(68, 33);
         // customer
         const customer = new CustomerDisplayerPrefab(this, 0, 0);
         customer_container.add(customer);
+        // mirror_container
+        const mirror_container = new Mirror(this, 55, 55);
+        this.add.existing(mirror_container);
         // mirror_scene_frame
-        this.add.image(400, 300, "mirror_scene_frame");
+        const mirror_scene_frame = this.add.image(0, 0, "mirror_scene_frame");
+        mirror_scene_frame.setOrigin(0, 0);
         // makeup
         const makeup = this.add.container(160, 494);
         // bottle_red
@@ -516,32 +541,32 @@ class Level extends Phaser.Scene {
         const timer_icon = this.add.image(0, 0, "timer_icon");
         timerContainer.add(timer_icon);
         // request_container
-        const request_container = this.add.container(633, 218);
+        const request_container = this.add.container(600, 200);
         request_container.scaleX = 0.5;
         request_container.scaleY = 0.5;
+        // sprite
+        const sprite = this.add.sprite(0, 0, "thought_bubble_1");
+        sprite.scaleX = 1.8;
+        sprite.scaleY = 1.8;
+        request_container.add(sprite);
         // request_head
         const request_head = this.add.image(0, 0, "customer_frank_head");
         request_container.add(request_head);
         // makeup_list
         const makeup_list = this.add.container(0, 0);
         request_container.add(makeup_list);
-        // image
-        const image = this.add.image(7, 30, "cutout");
-        image.scaleX = 1.5042186768858377;
-        image.scaleY = 1.206779112303157;
-        request_container.add(image);
         // text_whatwill
         const text_whatwill = this.add.text(639, 132, "", {});
         text_whatwill.setOrigin(0.5, 0.5);
         text_whatwill.text = "What will it be?";
         text_whatwill.setStyle({ "backgroundColor": "", "fontSize": "24px", "strokeThickness": 1, "shadow.offsetX": 3, "shadow.offsetY": 3, "shadow.stroke": true });
         // customer_foreground
-        const customer_foreground = this.add.image(280, 261, "customer_steve_head_back");
+        const customer_foreground = this.add.image(293, 274, "customer_steve_head_back");
         customer_foreground.setOrigin(0, 0);
         // grade_container
         const grade_container = this.add.container(426, 193);
         // grade_text
-        const grade_text = this.add.text(-400, -50, "", {});
+        const grade_text = this.add.text(-253, 37, "", {});
         grade_text.text = "S+";
         grade_text.setStyle({ "align": "center", "color": "#ef0000ff", "fixedWidth": 800, "fontSize": "128px", "fontStyle": "bold", "stroke": "#000000ff", "shadow.offsetX": 2, "shadow.offsetY": 2, "shadow.color": "#000000ff", "shadow.stroke": true, "shadow.fill": true });
         grade_container.add(grade_text);
@@ -571,10 +596,16 @@ class Level extends Phaser.Scene {
         bottle_blue_3_1.emit("components-awake");
         // timerContainer (components)
         const timerContainerTimer = new Timer(timerContainer);
-        timerContainerTimer.timer_length = 13;
+        timerContainerTimer.timer_length = 20;
         timerContainerTimer.icon = timer_icon;
         timerContainer.emit("components-awake");
+        // sprite (components)
+        const spriteStartAnimation = new StartAnimation(sprite);
+        spriteStartAnimation.animation_key = "animate";
+        sprite.emit("components-awake");
+        this.mirror_bg = mirror_bg;
         this.customer_container = customer_container;
+        this.mirror_container = mirror_container;
         this.bottle_red = bottle_red;
         this.timer_icon = timer_icon;
         this.request_container = request_container;
@@ -589,6 +620,7 @@ class Level extends Phaser.Scene {
     create() {
         this.shuffle(this.customers_ids);
         this.editorCreate();
+        this.mirror_bg.setDepth(-2);
         this.comparison_texture = this.add.renderTexture(0, 0, 400, 400);
         this.text_whatwill.visible = false;
         this.events.on(GameEvent.GAME_INTRO, this.introCustomer, this);
@@ -596,7 +628,7 @@ class Level extends Phaser.Scene {
         this.events.on(GameEvent.NEW_CUSTOMER, this.loopGame, this);
         this.events.on(GameEvent.GAME_END, this.onGameComplete, this);
         this.events.on(GameEvent.MAKEUP_SELECTED, this.onPaintSelected, this);
-        this.bottle_red.emit('pointerdown');
+        this.bottle_red.emit("pointerdown");
         this.createNewCustomer();
         this.events.emit(GameEvent.GAME_INTRO);
     }
@@ -608,7 +640,11 @@ class Level extends Phaser.Scene {
     }
     introCustomer() {
         this.customer_container.x = 700;
-        this.tweens.add({
+        this.customer_container.y = 33;
+        this.customer_container.setDepth(-1);
+        this.customer_foreground.visible = true;
+        let timeline = this.tweens.createTimeline();
+        timeline.add({
             duration: 1000,
             targets: this.customer_container,
             ease: Phaser.Math.Easing.Quintic.Out,
@@ -616,78 +652,140 @@ class Level extends Phaser.Scene {
             onUpdate: (tween, target) => {
                 this.customer_foreground.x = target.x + 250;
             },
-            completeDelay: 1000,
+            completeDelay: 1500,
             onComplete: () => {
                 this.text_whatwill.visible = false;
                 this.startDrawingGame();
             },
         });
+        timeline.add({
+            duration: 500,
+            targets: this.request_container,
+            ease: Phaser.Math.Easing.Quintic.Out,
+            scale: 0.5,
+            onStart: (tween, target) => {
+                this.request_container.x = 600;
+                this.request_container.y = 200;
+                this.request_container.visible = true;
+                this.request_container.scale = 0;
+            },
+            completeDelay: 4000,
+        });
+        timeline.add({
+            duration: 3000,
+            targets: this.request_container,
+            ease: Phaser.Math.Easing.Linear,
+            scale: 0,
+            onComplete: (tween, target) => {
+                this.request_container.visible = false;
+            },
+        });
         this.text_whatwill.visible = true;
-        // this.text_whatwill.rotation = -Math.PI / 16;
-        // this.tweens.add({
-        //   duration: 300,
-        //   targets: this.text_whatwill,
-        //   ease: Phaser.Math.Easing.Quintic.InOut,
-        //   rotation: Math.PI / 16,
-        //   yoyo: true,
-        //   repeat: 10,
-        // });
+        timeline.play();
+        this.current_timeline = timeline;
     }
     startDrawingGame() {
         this.events.emit(GameEvent.GAME_START);
     }
     onGameTimerComplete() {
+        if (this.current_timeline) {
+            this.current_timeline.stop();
+            this.request_container.visible = false;
+        }
+        // Get the score and use it later gator
         let score = this.activeCustomerPrefab.getMakeupScore(this.comparison_texture);
-        console.log('starting difference percent : ' + this.starting_score);
-        console.log('ending difference percent : ' + score);
         let grade = this.getGrade(this.starting_score, score);
         this.grades.push(grade);
-        this.grade_text.text = grade;
-        this.grade_container.visible = true;
-        this.grade_container.setScale(1.5);
-        this.tweens.add({
+        Phaser.Cameras.Scene2D.CameraManager;
+        let timeline = this.tweens.createTimeline();
+        timeline.add({
+            delay: 1500,
+            completeDelay: 1000,
+            duration: 1000,
+            targets: this.cameras.main,
+            ease: Phaser.Math.Easing.Linear,
+            scrollX: 250,
+            scrollY: 65,
+            onComplete: () => {
+            },
+        });
+        timeline.add({
             duration: 500,
             targets: this.grade_container,
             ease: Phaser.Math.Easing.Quintic.Out,
             scaleX: 1,
             scaleY: 1,
+            completeDelay: 3000,
+            onStart: () => {
+                this.customer_foreground.visible = false;
+                this.customer_container.x = this.customer_foreground.x;
+                this.customer_container.y = this.customer_foreground.y;
+                this.customer_container.setDepth(1);
+                this.grade_text.text = grade;
+                this.grade_container.visible = true;
+                this.grade_container.setScale(1.5);
+                this.request_container.visible = true;
+                this.request_container.scale = 0.5;
+                this.request_container.x = 880;
+                this.request_container.y = 520;
+            },
+            onComplete: () => {
+                this.events.emit(GameEvent.NEW_CUSTOMER);
+            },
         });
-        setTimeout(() => {
-            this.createNewCustomer();
-            this.events.emit(GameEvent.GAME_INTRO);
-        }, 1500);
-    }
-    getGrade(start_score, final_score) {
-        let pct = 100 - (final_score * (100 / start_score));
-        if (pct > 85) {
-            return 'S+';
-        }
-        else if (pct > 55) {
-            return 'A';
-        }
-        else if (pct > 35) {
-            return 'B';
-        }
-        else if (pct > 10) {
-            return 'C';
-        }
-        else if (pct > -5) {
-            return 'F';
-        }
-        else {
-            let options = [
-                'WHAT?',
-                ':^(',
-                'y?',
-                'no',
-                'Z',
-                'F-',
-                'no no no'
-            ];
-            return options[Math.floor(options.length * Math.random())];
-        }
+        timeline.play();
+        console.log("starting difference percent : " + this.starting_score);
+        console.log("ending difference percent : " + score);
+        this.current_timeline = timeline;
     }
     loopGame() {
+        this.current_timeline.stop();
+        this.request_container.visible = false;
+        let timeline = this.tweens.createTimeline();
+        timeline.add({
+            duration: 1000,
+            targets: this.customer_container,
+            ease: Phaser.Math.Easing.Expo.Out,
+            x: 1400,
+        });
+        timeline.add({
+            delay: 2000,
+            completeDelay: 1000,
+            duration: 1000,
+            targets: this.cameras.main,
+            ease: Phaser.Math.Easing.Linear,
+            scrollX: 0,
+            scrollY: 0,
+            onComplete: () => {
+                this.createNewCustomer();
+                if (this.customer_index <= this.customers_ids.length - 1) {
+                    this.events.emit(GameEvent.GAME_INTRO);
+                }
+            },
+        });
+        timeline.play();
+    }
+    getGrade(start_score, final_score) {
+        let pct = 100 - final_score * (100 / start_score);
+        if (pct > 85) {
+            return "S+";
+        }
+        else if (pct > 55) {
+            return "A";
+        }
+        else if (pct > 35) {
+            return "B";
+        }
+        else if (pct > 10) {
+            return "C";
+        }
+        else if (pct > -5) {
+            return "F";
+        }
+        else {
+            let options = ["WHAT?", ":^(", "y?", "no", "Z", "F-", "no no no"];
+            return options[Math.floor(options.length * Math.random())];
+        }
     }
     onGameComplete() { }
     // Util
@@ -721,18 +819,18 @@ class Level extends Phaser.Scene {
         this.customer_foreground.setTexture("customer_" + customer_id + "_head_back");
         let templates = [];
         for (let i = 1; i <= 10; i++) {
-            templates.push(customer_id + '_makeup_' + i);
+            templates.push(customer_id + "_makeup_" + i);
         }
         this.shuffle(templates);
         let amount = 2 + Math.round(Math.random() * 3);
         let requests = templates.slice(0, amount);
-        customer.setMakeupRequest(requests); //customer_id + '_makeup_' + index);
+        customer.setMakeupRequest(requests);
         customer.setBrush(this.active_paint);
         //show preview head
-        this.request_container.visible = true;
-        this.request_head.setTexture('customer_' + customer_id + '_head');
+        this.request_head.setTexture("customer_" + customer_id + "_head");
+        this.request_container.visible = false;
         this.makeup_list.removeAll(true);
-        requests.forEach(entry => {
+        requests.forEach((entry) => {
             let img = this.make.image({
                 x: 0,
                 y: 0,
@@ -744,13 +842,13 @@ class Level extends Phaser.Scene {
             this.makeup_list.add(img);
         });
         this.comparison_texture.clear();
-        this.request_container.setScale(1);
         this.comparison_texture.visible = false;
         this.comparison_texture.draw(this.makeup_list, 200, 200);
         this.request_container.setScale(0.5);
         this.starting_score = customer.getMakeupScore(this.comparison_texture);
         this.activeCustomerPrefab = customer;
         this.customer_container.add(customer);
+        this.customer_container.setPosition(68, 33);
     }
     selectNewCustomer() {
         this.customer_index++;
@@ -762,6 +860,39 @@ class Level extends Phaser.Scene {
     getCurrentCustomerId() {
         // return 'frank';
         return this.customers_ids[this.customer_index];
+    }
+}
+/* END OF COMPILED CODE */
+// You can write more code here
+// You can write more code here
+/* START OF COMPILED CODE */
+class Mirror extends Phaser.GameObjects.Container {
+    constructor(scene, x, y) {
+        super(scene, x, y);
+        // rectangle
+        const rectangle = scene.add.rectangle(0, 0, 400, 400);
+        rectangle.setOrigin(0, 0);
+        rectangle.isFilled = true;
+        rectangle.fillColor = 7627610;
+        this.add(rectangle);
+        // text
+        const text = scene.add.text(208, 197, "", {});
+        text.setOrigin(0.5, 0.5);
+        text.text = "Times up!";
+        text.setStyle({ "fontSize": "32px" });
+        this.add(text);
+        /* START-USER-CTR-CODE */
+        this.visible = false;
+        this.scene.events.on(GameEvent.GAME_INTRO, this.onIntro, this);
+        this.scene.events.on(GameEvent.TIMER_COMPLETE, this.onTimerComplete, this);
+        /* END-USER-CTR-CODE */
+    }
+    /* START-USER-CODE */
+    onIntro() {
+        this.visible = false;
+    }
+    onTimerComplete() {
+        this.visible = true;
     }
 }
 /* END OF COMPILED CODE */
